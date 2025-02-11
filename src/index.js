41a84ec09c7add180c56685d73b793140a9bc591
sia <sia@cfbtel.com>
2025-02-11 18:25:04 +0330

    about page

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import Engagebay from "./pages/engagebay/engagebay";
 import Mcdonald from "./pages/mcdonald/mcdonald";
 import Home from "./pages/home/home";
 import Counter from "./pages/counter/counter";
+import About from "./pages/about/about";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/mcdonald" element={<Mcdonald />} />
         <Route path="/engagebay" element={<Engagebay />} />
         <Route path="/calculator" element={<Calculator />} />
